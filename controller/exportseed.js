bf4c9dea5c5cde29ff3a34838a18bdcaaab87551
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-10-16 22:07:17 -0400

    working item seed

--- controller/exportseed.js
+++ controller/exportseed.js
@@ -69,7 +69,7 @@ var UsersObj,
 	exportSeedDataArray=[],
 	d = new Date(),
 	defaultExportDir = 'content/backups/seeds/',
-	defaultExportFileName = 'dbseed'+'-' + d.getUTCFullYear() + '-' + d.getUTCMonth() + '-' + d.getUTCDate()+'-' + d.getTime()+'.json';;
+	defaultExportFileName = 'dbseed'+'-' + d.getUTCFullYear() + '-' + d.getUTCMonth() + '-' + d.getUTCDate()+'-' + d.getTime()+'.json';
 /**
  * exports a seed data to seeds format
  * @param  {object} options - filepath,limits-tags,collections,etc
@@ -91,41 +91,172 @@ var writeSeedToDisk = function(writeSeedToDiskCallback){
 };
 
 /**
+ * return seed format for a tags object
+ * @return {object}     tags[name]
+ */
+var getTagsFromDoc = function(tags){
+	var tagsArray = [];
+	if(tags.length>0){
+		for(var ta=0 ; ta<tags.length; ta++){
+			if(tags[ta].name){
+				tagsArray.push(tags[ta].name);
+			}
+		}	
+	}
+	return tagsArray;
+};
+
+/**
+ * return seed format for a authors object
+ * @return {object}     authors[name]
+ */
+var getAuthorsFromDoc = function(authors){
+	var authorsArray = [];
+	if(authors.length>0){
+		for(var gafd=0 ; gafd<authors.length; gafd++){
+			if(authors[gafd].username){
+				authorsArray.push(authors[gafd].username);
+			}
+		}	
+	}
+	return authorsArray;
+};
+
+/**
+ * return seed format for a categories object
+ * @return {object}     categories[name]
+ */
+var getCategoriesFromDoc = function(categories){
+	var categoriesArray = [];
+	if(categories.length>0){
+		for(var gcfd=0 ; gcfd<categories.length; gcfd++){
+			if(categories[gcfd] && categories[gcfd].name){
+				categoriesArray.push(categories[gcfd].name);
+			}
+		}	
+	}
+	return categoriesArray;
+};
+
+/**
+ * return seed format for a contenttypes object
+ * @return {object}     contenttypes[name]
+ */
+var getContenttypesFromDoc = function(contenttypes){
+	var contenttypesArray = [];
+	if(contenttypes.length>0){
+		for(var gctfd = 0; gctfd<contenttypes.length; gctfd++){
+			if(contenttypes[gctfd].name){
+				contenttypesArray.push(contenttypes[gctfd].name);
+			}
+		}	
+	}
+	return contenttypesArray;
+};
+
+/**
+ * return seed format for a assets object
+ * @return {object}     assets[name]
+ */
+var getAssetsFromDoc = function(assets){
+	var assetsArray = [];
+	if(assets.length>0){
+		for(var gafd = 0; gafd<assets.length; gafd++){
+			if(assets[gafd].name){
+				assetsArray.push(assets[gafd].name);
+			}
+		}	
+	}
+	return assetsArray;
+};
+
+/**
+ * return seed format for a primary author object
+ * @return {object}     primaryauthor.name
+ */
+var getPrimaryAuthorFromDoc = function(primaryauthor){
+	return (primaryauthor && primaryauthor.username)? primaryauthor.username : '';
+};
+
+/**
+ * return seed format for a primary asset object
+ * @return {object}     primaryasset.name
+ */
+var getPrimaryAssetFromDoc = function(primaryasset){
+	return (primaryasset && primaryasset.name)? primaryasset.name : '';
+};
+
+/**
  * return seed format for an asset object
  * @param  {object} doc mongo document
  * @return {object}     seed object
  */
 var getItemSeed = function(doc){
-	var seed = {
-		datatype:'item'	
+	var returnseed = {
+		datatype:'item',
+		datadocument:{}
 	};
+	returnseed.datadocument.random = doc.random;
+	returnseed.datadocument.title = doc.title;
+	returnseed.datadocument.name = doc.name;
+	returnseed.datadocument.content = doc.content;
+	returnseed.datadocument.collectionitemonly = doc.collectionitemonly;
+	returnseed.datadocument.updatedat = doc.updatedat;
+	returnseed.datadocument.createdat = doc.createdat;
+	returnseed.datadocument.publishat = doc.publishat;
+	returnseed.datadocument.entitytype = doc.entitytype;
+	returnseed.datadocument.status = doc.status;
 
-	seed.datadocument=doc;
-	delete seed.datadocument._id;
-	return seed;
-};
-
-/**
- * create item seeds from the database, if there are assets
- * @param  {object} err
- * @param  {Function} createAssetSeedsAsyncCallback
- * @return {Function} async callback createAssetSeedsAsyncCallback(err,results);
- */
-var createItemSeeds = function(createItemSeedsAsyncCallback){
-	Item.find({}).select('-_id status entitytype publishat createdat updatedat title name content contenttypes tags categories assets primaryasset authors primaryauthor source collectionitemonly itemauthorname originalitem changes link visibility visiblitypassword contenttypeattributes extentionattributes random').populate('tags categories assets primaryasset authors primaryauthor').exec(function(err,Items){
-		if(err){
-			exportSeedErrorsArray.push({
-				error:err,
-				errortype:'createItemSeeds'
-			});
-		}
-		if(Items){
-			for(var i in Items){
-				exportSeedDataArray.push(getItemSeed(Items[i]));
-			}
-		}
-		createItemSeedsAsyncCallback(null,'created item seeds');
-	});
+	if(typeof doc.contenttypeattributes!=='undefined'){
+		returnseed.datadocument.contenttypeattributes = doc.contenttypeattributes;
+	}
+	if(typeof doc.itemauthorname!=='undefined'){
+		returnseed.datadocument.itemauthorname = doc.itemauthorname;
+	}
+	if(doc.primaryauthor){
+		returnseed.datadocument.primaryauthor = getPrimaryAuthorFromDoc(doc.primaryauthor);
+	}
+	if(doc.primaryasset){
+		returnseed.datadocument.primaryasset = getPrimaryAssetFromDoc(doc.primaryasset);
+	}
+	if(doc.contenttypeattributes){
+		returnseed.datadocument.contenttypeattributes = doc.contenttypeattributes;
+	}
+	if(doc.link){
+		returnseed.datadocument.link = doc.link;
+	}
+	if(doc.changes){
+		returnseed.datadocument.changes = doc.changes;
+	}
+	if(doc.tags){
+		returnseed.datadocument.tags = getTagsFromDoc(doc.tags);
+	}
+	if(doc.categories){
+		returnseed.datadocument.categories = getCategoriesFromDoc(doc.categories);
+	}
+	if(doc.contenttypes){
+		returnseed.datadocument.contenttypes = getContenttypesFromDoc(doc.contenttypes);
+	}
+	if(doc.authors){
+		returnseed.datadocument.authors = getAuthorsFromDoc(doc.authors);
+	}
+	if(doc.assets){
+		returnseed.datadocument.assets = getAssetsFromDoc(doc.assets);
+	}
+	if(doc.visibility){
+		returnseed.datadocument.visibility = doc.visibility;
+	}
+	if(doc.visibilitypassword){
+		returnseed.datadocument.visibilitypassword = doc.visibilitypassword;
+	}
+	if(doc.extensionattributes){
+		returnseed.datadocument.extensionattributes = doc.extensionattributes;
+	}
+	if(doc.originalitem){
+		returnseed.datadocument.originalitem = doc.originalitem;
+	}
+			
+	return returnseed;
 };
 
 /**
@@ -134,13 +265,50 @@ var createItemSeeds = function(createItemSeedsAsyncCallback){
  * @return {object}     seed object
  */
 var getAssetSeed = function(doc){
-	var seed = {
-		datatype:'asset'	
+	var returnseed = {
+		datatype:'asset',
+		datadocument:{
+			random:doc.random,
+			primaryauthor:getPrimaryAuthorFromDoc(doc.primaryauthor)
+		}
 	};
+	// try{
+	// 	seed.datadocument.primaryauthor = getPrimaryAuthorFromAuthorDoc(doc.primaryauthor);
+	// }
+	// catch(e){
+	// 	exportSeedErrorsArray.push({
+	// 		error:e,
+	// 		errortype:'getAssetSeed'
+	// 	});
+	// 	seed.datadocument=e;
+	// }
+	console.log(returnseed);
+	return returnseed;
+};
 
-	seed.datadocument=doc;
-	delete seed.datadocument._id;
-	return seed;
+
+/**
+ * create item seeds from the database, if there are assets
+ * @param  {object} err
+ * @param  {Function} createAssetSeedsAsyncCallback
+ * @return {Function} async callback createAssetSeedsAsyncCallback(err,results);
+ */
+var createItemSeeds = function(createItemSeedsAsyncCallback){
+	Item.find({}).select('-_id status entitytype publishat createdat updatedat title name content contenttypes tags categories assets primaryasset authors primaryauthor source collectionitemonly itemauthorname originalitem changes link visibility visiblitypassword contenttypeattributes extentionattributes random').populate('tags categories assets primaryasset authors contenttypes primaryauthor').exec(function(err,Items){
+		if(err){
+			exportSeedErrorsArray.push({
+				error:err,
+				errortype:'createItemSeeds'
+			});
+		}
+		if(Items){
+			for(var i in Items){
+				var itemdoc = Items[i];
+				exportSeedDataArray.push(getItemSeed(itemdoc));
+			}
+		}
+		createItemSeedsAsyncCallback(null,'created item seeds');
+	});
 };
 
 /**
@@ -150,7 +318,7 @@ var getAssetSeed = function(doc){
  * @return {Function} async callback createAssetSeedsAsyncCallback(err,results);
  */
 var createAssetSeeds = function(createAssetSeedsAsyncCallback){
-	Asset.find({}).select('-_id title name status createdat author entitytype userid username assettype contenttypes.name fileurl locationtype description content filedata attributes contenttypeattributes extentionattributes random').populate('author contenttypes').exec(function(err,Assets){
+	Asset.find({}).select('-_id title name status createdat author entitytype userid username assettype contenttypes fileurl locationtype description content filedata attributes contenttypeattributes extentionattributes random').populate('author contenttypes').exec(function(err,Assets){
 		if(err){
 			exportSeedErrorsArray.push({
 				error:err,
@@ -159,7 +327,8 @@ var createAssetSeeds = function(createAssetSeedsAsyncCallback){
 		}
 		if(Assets){
 			for(var a in Assets){
-				exportSeedDataArray.push(getAssetSeed(Assets[a]));
+				var assetdoc = Assets[a];
+				exportSeedDataArray.push(getAssetSeed(assetdoc));
 			}
 		}
 		createAssetSeedsAsyncCallback(null,'created asset seeds');
