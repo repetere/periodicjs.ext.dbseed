883453a098ec832d39570109c51a0076321fd64d
Yaw Joseph Etse <yaw.etse@gmail.com>
2017-06-15 18:00:25 -0400

    adding tests for dbseed

--- test/unit/periodic_extension_spec.js
+++ test/unit/periodic_extension_spec.js
@@ -1,18 +1,16 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs-extra');
+const flatten = require('flat');
 const events = require('events');
 const chai = require('chai');
 const sinon = require('sinon');
-const fs = require('fs-extra');
 const expect = require('chai').expect;
 const extension_root_dir = path.resolve(__dirname, '../../');
+let extension_files=[];
 chai.use(require('sinon-chai'));
 require('mocha-sinon');
-
-const EXTENSION_commands = require(path.join(extension_root_dir, 'commands/index'));
-const EXTENSION_config = require(path.join(extension_root_dir, 'config/settings'));
-
 
 describe('Valid Periodic extension', function () {
   this.timeout(10000);
@@ -36,6 +34,7 @@ describe('Valid Periodic extension', function () {
       let requiredStructure = standardExtensionStructure.length;
       fs.readdir(extension_root_dir)
         .then(files => {
+          extension_files = files;
           files.forEach(file => {
             if (standardExtensionStructure.indexOf(file.toString()) > -1) {
               requiredStructure--;
@@ -48,13 +47,50 @@ describe('Valid Periodic extension', function () {
         .catch(done);
     });
     it('should export asynchronous tasks', () => {
-      Object.keys(EXTENSION_commands).forEach(command => {
-        expect(EXTENSION_commands[ command ]).to.be.a('function');
-      });
+      if (extension_files.indexOf('commands') > -1) {
+        const EXTENSION_commands = require(path.join(extension_root_dir, 'commands/index'));
+        Object.keys(EXTENSION_commands).forEach(command => {
+          expect(EXTENSION_commands[ command ]).to.be.a('function');
+        });
+      } else {
+        console.log('Extension has no commands');
+      }
     });
     it('should export default config, with settings and databases', () => {
+      const EXTENSION_config = require(path.join(extension_root_dir, 'config/settings'));
       expect(EXTENSION_config.settings).to.be.an('object');
       expect(EXTENSION_config.databases).to.be.an('object');
-    })
+    });
+    it('should export valid utilities', () => {
+      const EXTENSION_utilities = require(path.join(extension_root_dir, 'utilities/index'));
+      expect(EXTENSION_utilities).to.be.an('object');
+    });
+    it('should export a valid express router', () => {
+      if(extension_files.indexOf('routers') >-1 ){
+        const EXTENSION_routers = require(path.join(extension_root_dir,   'routers/index'));
+        expect(EXTENSION_routers).to.be.a('function');
+        expect(EXTENSION_routers).to.have.a.property('stack');
+        expect(EXTENSION_routers).to.have.a.property('params');
+      } else {
+        console.log('Extension has no routes');
+      }
+    });
+    it('should export valid functions from transforms', () => {
+      if (extension_files.indexOf('transforms') > -1) {
+        const EXTENSION_transforms = require(path.join(extension_root_dir, 'transforms/index'));
+        const flattened_transforms = flatten(EXTENSION_transforms);
+        Object.keys(flattened_transforms).forEach(transform => {
+          if (Object.keys(flattened_transforms[ transform ]).length) {
+            expect(flattened_transforms[ transform ]).to.be.a('function');
+          }
+        });
+      } else {
+        console.log('Extension has no transforms');
+      }
+    });
+    it('should export a valid node module',()=>{
+      const EXTENSION_module = require(path.join(extension_root_dir, 'index'));
+      expect(EXTENSION_module).to.be.a('function');
+    });
   });
 });
