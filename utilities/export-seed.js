ee180bde4e29422148bfc8eb86feb48e8b9ec3e6
Jan Bialostok <janbialostok@gmail.com>
2017-09-06 16:40:25 -0400

    export seed working with partitions

--- utilities/export-seed.js
+++ utilities/export-seed.js
@@ -1,9 +1,10 @@
 'use strict';
 
-const fs = require('fs-extra');
+const Promisie = require('promisie');
+const fs = Promisie.promisifyAll(require('fs-extra'));
 const path = require('path');
 const periodicjs = require('periodicjs');
-const Promisie = require('promisie');
+const Transform = require('stream').Transform;
 
 /**
  * exports data from a given core data database model
@@ -11,20 +12,88 @@ const Promisie = require('promisie');
  * @param {string} core_data_name 
  * @returns {Promise} contents of the core data model seed
  */
-function exportCoreData(core_data_name) {
-  return new Promise((resolve, reject) => {
-    try {
-      periodicjs.datas.get(core_data_name).query()
-        .then(data => {
-          resolve({
-            [ core_data_name ]: data,
-          });
-        })
-        .catch(reject);
-    } catch (e) {
-      reject(e);
+function handleWriteStream (fd) {
+  let isFirst = true;
+  let transform = new Transform({
+    objectMode: true,
+    transform: function (data, enc, next) {
+      try {
+        if (isFirst) {
+          isFirst = false;
+          this.push('[');
+        }
+        this.push((data && typeof data === 'object') ? JSON.stringify(data) : data);
+        next();
+      } catch (e) {
+        next(e);
+      }
     }
   });
+  transform.pipe(fd);
+  return transform;
+}
+
+function handleReadStream (core_data_name, writeStream, fd) {
+  let firstData = true;
+  return function (readStream) {
+    let count = 0;
+    return new Promisie((resolve, reject) => {
+      try {
+        writeStream.write(`{"${ core_data_name }": [`);
+        writeStream.once('error', reject);
+        readStream.on('data', data => {
+          count++;
+          if (firstData) {
+            firstData = false;
+            writeStream.write(data);
+          } else {
+            writeStream.write(',');
+            writeStream.write(data);
+          }
+        })
+          .on('error', reject)
+          .on('end', () => {
+            writeStream.write(']');
+            resolve([writeStream, fd, count]);
+          });
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+}
+
+function handleWriteEnd (transform, fd) {
+  transform.end('}]');
+  return new Promisie((resolve, reject) => {
+    fd.on('finish', () => resolve())
+      .on('error', reject);
+  });
+}
+
+function exportCoreData (filepath, split_count) {
+  return function (core_data_name) {
+    let data_count = 0;
+    let file_index = 0;
+    return Promisie.doWhilst(() => {
+      let fd = fs.createWriteStream(path.join(path.dirname(filepath), `${ path.basename(filepath, '.json') }_${ core_data_name }(${ file_index }).json`));
+      let writeStream = handleWriteStream(fd);
+      return periodicjs.datas.get(core_data_name).stream({ 
+        limit: (typeof split_count === 'number') ? split_count : Infinity,
+        skip: data_count
+      })
+        .then(handleReadStream(core_data_name, writeStream, fd))
+        .then(result => {
+          let [transform, fd, count] = result;
+          data_count += count;
+          file_index++;
+          return handleWriteEnd(transform, fd)
+            .then(() => count)
+            .catch(e => Promisie.reject(e));
+        })
+        .catch(e => Promisie.reject(e));
+    }, result => (typeof split_count === 'number' && split_count > 0 && result === split_count));
+  }
 }
 
 /**
@@ -33,23 +102,15 @@ function exportCoreData(core_data_name) {
  * @param {string} filepath 
  * @returns {Promise} resolved value from each export from exportCoreData
  */
-function exportData(filepath) {
-  return new Promise((resolve, reject) => {
-    try {
-      const excluded_data = periodicjs.settings.extensions['periodicjs.ext.dbseed'].export.ignore_core_datas;
-      const core_datas = Array.from(periodicjs.datas.keys()).filter(datum => excluded_data.indexOf(datum) === -1);
-      fs.ensureFile(filepath)
-        .then(() => {
-          return Promisie.map(core_datas, 5, exportCoreData);
-        })
-        .then(datas => {
-          resolve(fs.outputJSON(filepath, datas));
-        })
-        .catch(reject);
-    } catch (e) {
-      reject(e);
-    }
-  });
+function exportData (filepath) {
+  try {
+    const excluded_data = periodicjs.settings.extensions['periodicjs.ext.dbseed'].export.ignore_core_datas;
+    const split_count = periodicjs.settings.extensions['periodicjs.ext.dbseed'].export.split_count;
+    const core_datas = Array.from(periodicjs.datas.keys()).filter(datum => excluded_data.indexOf(datum) === -1);
+    return Promisie.map(core_datas, 5, exportCoreData(filepath, split_count));
+  } catch (e) {
+    return Promisie.reject(e);
+  }
 }
 
 module.exports = {
